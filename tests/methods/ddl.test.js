83aba8a23b66f340c991580d6b7d77b01e521efa
Ryanbarker0 <ryangeorgebarker0@gmail.com>
2019-09-30 18:23:23 +0100

    new: tests + ddl change

--- tests/methods/ddl.test.js
+++ tests/methods/ddl.test.js
@@ -4,13 +4,15 @@ const obj = {
 	data: [
 		{
 			attributes: {
-				name: 'Ryan',
+				first_name: 'Ryan',
+				last_name: 'Barker'
 			},
 			id: 50,
 		},
 		{
 			attributes: {
-				name: 'Adam',
+				first_name: 'Adam',
+				last_name: 'Barker'
 			},
 			id: 51,
 		}
@@ -18,7 +20,7 @@ const obj = {
 };
 
 test('It should correctly get text & value based paths passed in', () => {
-	expect(ddl.standard(obj.data, 'attributes.name', 'id')).toEqual({
+	expect(ddl.standard(obj.data, 'attributes.first_name', 'id')).toEqual({
 		result: [
 			{
 				text: 'Ryan',
@@ -32,8 +34,8 @@ test('It should correctly get text & value based paths passed in', () => {
 	});
 });
 
-test('It should correct get text & value based on mustached values', () => {
-	expect(ddl.mustached(obj.data, '{{attributes.name}}', '{{id}}')).toEqual({
+test('It should correctly get text & value based on mustached values', () => {
+	expect(ddl.mustached(obj.data, '{{attributes.first_name}}', '{{id}}')).toEqual({
 		result: [
 			{
 				text: 'Ryan',
@@ -42,6 +44,21 @@ test('It should correct get text & value based on mustached values', () => {
 			{
 				text: 'Adam',
 				value: '51',
+			},
+		]
+	});
+});
+
+test('It should correctly get IDs as integer if set to true', () => {
+	expect(ddl.mustached(obj.data, '{{attributes.first_name}} {{attributes.last_name}}', 'id', true)).toEqual({
+		result: [
+			{
+				text: 'Ryan Barker',
+				value: 50,
+			},
+			{
+				text: 'Adam Barker',
+				value: 51,
 			},
 		]
 	});
---
+++ tests/methods/lookup.test.js
@@ -0,0 +1,37 @@
+const lookup = require('../../lib/lookup');
+describe('Lookup objects should be correctly outputted', () => {
+
+	test('It should output a basic lookup object if only a message is passed', () => {
+		expect(lookup({ message: 'run_operation' })).toEqual({
+			url: '{{{step.ephemeral_url}}}',
+			body: {
+				auth_id: '{{{step.auth_id}}}',
+				step_settings: {},
+				message: 'run_operation',
+			},
+		});
+	});
+
+	test('It should add custom step_settings if set', () => {
+		expect(lookup({ message: 'run_operation',
+			step_settings: {
+				auth: {
+					type: 'jsonpath',
+					value: '$.auth'
+				}
+			}
+		}		)).toEqual({
+			url: '{{{step.ephemeral_url}}}',
+			body: {
+				auth_id: '{{{step.auth_id}}}',
+				step_settings: {
+					auth: {
+						type: 'jsonpath',
+						value: '$.auth'
+					}
+				},
+				message: 'run_operation',
+			}
+		});
+	});
+});
