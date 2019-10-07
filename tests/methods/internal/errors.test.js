289fe2e7a71d4f103300bbff6765e7a8d2e279b7
Ryanbarker0 <ryangeorgebarker0@gmail.com>
2019-10-07 16:41:51 +0100

    new: tests - coverage at 100pct

---
+++ tests/methods/internal/errors.test.js
@@ -0,0 +1,16 @@
+const { XMLError } = require('../../../lib/internal/errors');
+
+describe('Tests should correctly throw XML errors', () => {
+	test('It should throw a basic XML error with default message', () => {
+		const err = () => {
+			throw new XMLError();
+		};
+		expect(err).toThrow(XMLError);
+	});
+	test('It should throw a basic XML error with a custom message', () => {
+		const err = () => {
+			throw new XMLError('Custom error');
+		};
+		expect(err).toThrow('Custom error');
+	});
+});
