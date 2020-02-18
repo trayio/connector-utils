669458e7023be77f49a5180bbba0707a2c78bc30
Ryanbarker0 <ryangeorgebarker0@gmail.com>
2020-02-18 16:05:28 +0000

    new: add negative validation + improve naming

--- tests/methods/validatePagination.test.js
+++ tests/methods/validatePagination.test.js
@@ -1,4 +1,4 @@
-const { validatePagination } = require('../../');
+const { validatePaginationRange } = require('../../');
 
 const PAGE_SIZE = 50;
 const MIN_PAGE_SIZE = 1;
@@ -7,37 +7,57 @@ const MAX_PAGE_SIZE = 100;
 describe('Pagination should be correctly validated', () => {
 	test('No error should be thrown if pageSize is within range.', () => {
 		expect(
-			validatePagination(PAGE_SIZE, MIN_PAGE_SIZE, MAX_PAGE_SIZE),
+			validatePaginationRange(PAGE_SIZE, MIN_PAGE_SIZE, MAX_PAGE_SIZE),
 		).toBe(undefined);
 	});
 	test('Error should be thrown if the pageSize is larger than required range.', () => {
 		const LARGE_PAGE_SIZE = 101;
 		expect(() =>
-			validatePagination(LARGE_PAGE_SIZE, MIN_PAGE_SIZE, MAX_PAGE_SIZE),
+			validatePaginationRange(
+				LARGE_PAGE_SIZE,
+				MIN_PAGE_SIZE,
+				MAX_PAGE_SIZE,
+			),
 		).toThrow('The page size must be between 1 - 100.');
 	});
 	test('Error should be thrown if the pageSize is smaller than required range', () => {
 		const SMALL_PAGE_SIZE = 0;
 		expect(() =>
-			validatePagination(SMALL_PAGE_SIZE, MIN_PAGE_SIZE, MAX_PAGE_SIZE),
+			validatePaginationRange(
+				SMALL_PAGE_SIZE,
+				MIN_PAGE_SIZE,
+				MAX_PAGE_SIZE,
+			),
 		).toThrow('The page size must be between 1 - 100.');
 	});
 	test('Error should be thrown when pageSize is not an integer', () => {
 		const STRING_PAGE_SIZE = '50';
 		expect(() =>
-			validatePagination(STRING_PAGE_SIZE, MIN_PAGE_SIZE, MAX_PAGE_SIZE),
+			validatePaginationRange(
+				STRING_PAGE_SIZE,
+				MIN_PAGE_SIZE,
+				MAX_PAGE_SIZE,
+			),
 		).toThrow('Arguments need to be integer types.');
 	});
 	test('Error should be thrown when minPageSize is not an integer', () => {
 		const STRING_MIN_PAGE_SIZE = '1';
 		expect(() =>
-			validatePagination(PAGE_SIZE, STRING_MIN_PAGE_SIZE, MAX_PAGE_SIZE),
+			validatePaginationRange(
+				PAGE_SIZE,
+				STRING_MIN_PAGE_SIZE,
+				MAX_PAGE_SIZE,
+			),
 		).toThrow('Arguments need to be integer types.');
 	});
 	test('Error should be thrown when maxPageSize is not an integer', () => {
 		const STRING_MAX_PAGE_SIZE = '100';
 		expect(() =>
-			validatePagination(PAGE_SIZE, MIN_PAGE_SIZE, STRING_MAX_PAGE_SIZE),
+			validatePaginationRange(
+				PAGE_SIZE,
+				MIN_PAGE_SIZE,
+				STRING_MAX_PAGE_SIZE,
+			),
 		).toThrow('Arguments need to be integer types.');
 	});
 	test('Error should be thrown when all arguments are not integers', () => {
@@ -45,7 +65,7 @@ describe('Pagination should be correctly validated', () => {
 		const STRING_MIN_PAGE_SIZE = '1';
 		const STRING_MAX_PAGE_SIZE = '100';
 		expect(() =>
-			validatePagination(
+			validatePaginationRange(
 				STRING_PAGE_SIZE,
 				STRING_MIN_PAGE_SIZE,
 				STRING_MAX_PAGE_SIZE,
