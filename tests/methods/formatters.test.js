f91f52ee7ab11553ef314e51bf5ea818a14e6e46
David Thai <david.thai@tray.io>
2020-05-14 13:20:02 +0100

    error test

--- tests/methods/formatters.test.js
+++ tests/methods/formatters.test.js
@@ -1,14 +1,25 @@
+const { UserInputError } = require('../../lib/errors');
 const { formatArray, formatArrayToDelimitedList } = require('../../lib/index');
 
-describe('Array should be correctly formatted as a Delimited List', () => {
+describe('Array should be correctly formatted as a delimited list', () => {
 	describe('formatArray', () => {
 		it('should return a comma delimited list, given an array of strings', () => {
 			const arrayOfStrings = [1, 2, 'third', 'fourth'];
 			expect(formatArray(arrayOfStrings)).toEqual('1,2,third,fourth');
 		});
 
-		it('should return "undefined" if argument is undefined or not an array', () => {
+		it('should return "undefined" if argument is undefined', () => {
 			expect(formatArray(undefined)).toBeUndefined();
+		});
+
+		it('should throw error if input exists but is not an array', () => {
+			const notArray = 'not an array';
+
+			expect(() => formatArray(notArray)).toThrow(
+				new UserInputError(
+					"Expected an array but instead received input of type 'string'.",
+				),
+			);
 		});
 	});
 
@@ -30,10 +41,22 @@ describe('Array should be correctly formatted as a Delimited List', () => {
 			).toEqual('1//2//third//fourth');
 		});
 
-		it('should return "undefined" if value of "arrayToFormat" is undefined or not an array', () => {
+		it('should return "undefined" if value of "arrayToFormat" is undefined', () => {
 			expect(
 				formatArrayToDelimitedList({ arrayToFormat: undefined }),
 			).toBeUndefined();
+		});
+
+		it('should throw error if input exists but is not an array', () => {
+			const notArray = {};
+
+			expect(() =>
+				formatArrayToDelimitedList({ arrayToFormat: notArray }),
+			).toThrow(
+				new UserInputError(
+					"Expected an array but instead received input of type 'object'.",
+				),
+			);
 		});
 	});
 });
