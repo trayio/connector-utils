81c168c3a54e4811d7e2b271c4f9052c3378e1ba
Kianna Love <55241406+akirakianna@users.noreply.github.com>
2022-11-30 17:25:35 +0000

    Add prettyTitle helper function (originally from falafel) and tests, so that we are able to use it within connectors for formatting dynamic DDL text values etc. I also installed mout package, as that is what this function was using in falafel.

---
+++ tests/methods/prettyTitle.test.js
@@ -0,0 +1,64 @@
+const assert = require('assert');
+const prettyTitle = require('../../lib/prettyTitle');
+
+describe('#prettyTitle', () => {
+	it('should make names look nice', () => {
+		assert.strictEqual(prettyTitle('list_name'), 'List name');
+		assert.strictEqual(prettyTitle('list-name'), 'List name');
+		assert.strictEqual(prettyTitle('listName'), 'List name');
+		assert.strictEqual(prettyTitle('ListName'), 'List name');
+		assert.strictEqual(prettyTitle('listName'), 'List name');
+	});
+
+	it('should upper case id', () => {
+		assert.strictEqual(prettyTitle('id'), 'ID');
+		assert.strictEqual(prettyTitle('list_id'), 'List ID');
+		assert.strictEqual(prettyTitle('list_id_name'), 'List ID name');
+
+		assert.strictEqual(prettyTitle('lid'), 'Lid');
+		assert.strictEqual(prettyTitle('list baseid'), 'List baseid');
+	});
+
+	it('should upper case ids', () => {
+		assert.strictEqual(prettyTitle('ids'), 'IDs');
+		assert.strictEqual(prettyTitle('list_ids'), 'List IDs');
+		assert.strictEqual(prettyTitle('list_ids_name'), 'List IDs name');
+
+		assert.strictEqual(prettyTitle('lids'), 'Lids');
+		assert.strictEqual(prettyTitle('list baseids'), 'List baseids');
+	});
+
+	it('should upper case url', () => {
+		assert.strictEqual(prettyTitle('url'), 'URL');
+		assert.strictEqual(prettyTitle('list_url'), 'List URL');
+		assert.strictEqual(prettyTitle('list_url_name'), 'List URL name');
+
+		assert.strictEqual(prettyTitle('lurl'), 'Lurl');
+		assert.strictEqual(prettyTitle('list baseurl'), 'List baseurl');
+	});
+
+	it('should upper case ddl', () => {
+		assert.strictEqual(prettyTitle('ddl'), 'DDL');
+		assert.strictEqual(prettyTitle('list_ddl'), 'List DDL');
+
+		assert.strictEqual(prettyTitle('abcddl'), 'Abcddl');
+		assert.strictEqual(prettyTitle('list abcddl'), 'List abcddl');
+	});
+
+	it('should globally upper case id, ids and url', () => {
+		assert.strictEqual(prettyTitle('list_id_url_name'), 'List ID URL name');
+		assert.strictEqual(prettyTitle('list_url_id_name'), 'List URL ID name');
+		assert.strictEqual(
+			prettyTitle('list_ids_url_name'),
+			'List IDs URL name',
+		);
+		assert.strictEqual(
+			prettyTitle('list_url_ids_name'),
+			'List URL IDs name',
+		);
+		assert.strictEqual(
+			prettyTitle('list_url_ids_name_ddl'),
+			'List URL IDs name DDL',
+		);
+	});
+});
